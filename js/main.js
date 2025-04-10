917099e54f05b64af0ff73333a2a28b025f404b2
lorena2119 <189173816+lorena2119@users.noreply.github.com>
2025-04-09 22:05:35 -0500

    feat: :sparkles: Obtener datos de la API

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 // Variables
 const urlPersonajes = 'https://dattebayo-api.onrender.com/characters';
 const cardsContainer = document.getElementById('cards-container');
+let perPorOagina= 4
+const paginas = document.getElementById('pagination');
 let personajes = []
 let actpagina = 1;
+
+
+//Obtener personajes
+async function obtenerPersonajes() {
+    try{
+        const response = await fetch(urlPersonajes);
+        const res = await response.json();
+        personajes = res.characters;
+        paginacion(actpagina)
+    }catch(error){
+        console.log(error);
+    }
+}
