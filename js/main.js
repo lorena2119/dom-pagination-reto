763207f874dacbf7441575f3f777b24ee785f235
lorena2119 <189173816+lorena2119@users.noreply.github.com>
2025-04-09 22:52:16 -0500

    feat: :sparkles: Habilitar estilos de los botones activos

--- js/main.js
+++ js/main.js
@@ -23,11 +23,13 @@ function paginacion(pagina){
     if (pagina <1 || pagina > 5){
         return;
     }
-    currentPage = pagina;
+    actpagina = pagina;
     let start = (pagina - 1) * perPorPagina;
     let end = start + perPorPagina;
     let actPersonajes = personajes.slice(start, end);
-    actPersonajes.innerHTML = '';
+    cardsContainer.innerHTML = '';
+
+    // Crear card
     actPersonajes.forEach(personaje => {
         let card = document.createElement('div');
         card.classList.add('card');
@@ -35,12 +37,25 @@ function paginacion(pagina){
             <img class="card-image" src="${personaje.images[0]}" alt="">
             <div class="card-content">
                 <h2 class="card-title">${personaje.name}</h2>
-                <p class="card-clan">${personaje.clan}</p>
-                <p class="card-team">${personaje.team[0]}</p>
+                <p class="card-clan">${personaje.personal?.clan || 'None'}</p>
+                <p class="card-team">${personaje.debut?.manga}</p>
             </div>
         `;
         cardsContainer.appendChild(card);
     });
+
+    //Deshabilitar estilos "active" de los botones
+    document.querySelectorAll('#pagination button').forEach(button => button.classList.remove("active"))
+    const bton = document.getElementById(`${pagina}-button`);
+    if (bton){
+        bton.classList.add("active");
+    }
+    for (let i = 1; i <= 5; i++){
+        const bton = document.getElementById(`${i}-button`);
+        bton.addEventListener('click', () => paginacion(i));
+    }
+
 }
+
 
 obtenerPersonajes()
