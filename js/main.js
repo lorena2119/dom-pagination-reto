11f2456d7f713763f021a5591b7b3daa4d58ab8c
lorena2119 <189173816+lorena2119@users.noreply.github.com>
2025-04-09 21:59:55 -0500

    feat: :sparkles: Variables iniciales

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// Variables
+const urlPersonajes = 'https://dattebayo-api.onrender.com/characters';
+const cardsContainer = document.getElementById('cards-container');
+let personajes = []
+let actpagina = 1;
